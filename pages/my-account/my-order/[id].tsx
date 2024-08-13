728643a35e4b2ef8fb9ae8313d76933adffc54d3
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-08-13 04:00:37 +0100

    new change

--- pages/my-account/my-order/[id].tsx
+++ pages/my-account/my-order/[id].tsx
@@ -19,6 +19,7 @@ import { FaCheckCircle } from "react-icons/fa";
 import { FaPencil } from "react-icons/fa6";
 import { FaUserAlt } from "react-icons/fa";
 import { MyAccountNav } from "@/components/myAccountNav/myAccountNav";
+import { GoDotFill } from "react-icons/go";
 export default function PropertyDetails() {
 
     const propertyDetails: propertyOverview =  {
@@ -40,27 +41,33 @@ export default function PropertyDetails() {
     <MobileNav />
     <DesktopNav />
     <div className="px-[30px] py-[100px]">
-        <div className="grid gap-[50px] grid-cols-7 lg:grid-cols-4">
+        <div className="grid gap-[50px] grid-cols-10 lg:grid-cols-4">
             <MyAccountNav />
             <div></div>
-                <div className="col-span-5 lg:col-span-3 flex flex-col px-[20px] gap-5 h-fit py-[30px]">
+                <div className="col-span-9 lg:col-span-3 flex flex-col px-[20px] gap-5 h-fit py-[30px]">
                 <h1 className="bg-titleBg w-fit px-[20px] py-[10px] text-btn-primary text-[20px] uppercase font-semibold">
                             My Order
                         </h1>
-       <div className="  py-[20px] grid grid-cols-1 ">
+                    <div className="  py-[20px] grid grid-cols-1 ">
+                        <div className="flex gap-5 justify-between pb-[20px] items-center">
+                            <div className="flex  items-center"> <GoDotFill className="text-btn-primary mr-1" /> Pending</div>
+                            <button  className="bg-btn-primary text-light min-w-[40px] p-1 text-[5px] md:text-[13px]  font-semibold">
+                                                Cancel Order
+                                            </button>
+                        </div>
             <ImageSlider housing={propertyDetails} />
             
             <div className="grid gap-[100px] grid-cols-1 pt-[50px] ">
 
                 <div className="flex flex-col gap-[50px] ">
-                <div className="flex gap-5 ">
-                <span className="flex items-center gap-1"><FaLocationDot className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Enugu, Nigeria</p></span>
-                    <span className="flex items-center gap-1"><MdReviews className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">40 Reviews</p></span>
-                    <span className="flex items-center gap-2"><FaCalendarAlt className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]"/><p className="font-medium capitalize text-[15px] text-textColor">August 10, 2024</p></span>
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-2 ">
+                <span className="flex items-center gap-1"><FaLocationDot className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[10px] md:text-[15px] text-textColor">Enugu, Nigeria</p></span>
+                    <span className="flex items-center gap-1"><MdReviews className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[10px] md:text-[15px] text-textColor">40 Reviews</p></span>
+                    <span className="flex items-center gap-2"><FaCalendarAlt className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]"/><p className="font-medium capitalize text-[10px] md:text-[15px] text-textColor">August 10, 2024</p></span>
                     </div>
                     <div className="flex flex-col gap-[30px]">
-                        <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[30px] ">Property Description</h1>
-                        <p className="text-textColor text-[15px] md:text-[20px] ">
+                        <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[15px] md:text-[30px] ">Property Description</h1>
+                        <p className="text-textColor text-[12px] md:text-[20px] ">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
 
 Setting the mood with incense
@@ -87,30 +94,30 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur expedita v
                     </div>
 
                     <div className="flex flex-col gap-[30px] ">
-                        <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[30px] ">Property Detail</h1>
+                        <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[15px] md:text-[30px] ">Property Detail</h1>
                         <div className="flex flex-col bg-secondaryBg gap-5 rounded px-[20px] py-[30px] ">
-                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[20px]">Rooms : </h1> <p className="font-medium capitalize text-[15px] text-textColor">40</p></span>
-                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[20px]">Baths : </h1> <p className="font-medium capitalize text-[15px] text-textColor">40</p></span>
-                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[20px]">Year built : </h1> <p className="font-medium capitalize text-[15px] text-textColor">2024</p></span>
-                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[20px]"> Beds : </h1> <p className="font-medium capitalize text-[15px] text-textColor">32</p></span>
-                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[20px]">Price : </h1> <p className="font-medium capitalize text-[15px] text-textColor">N 400000</p></span>
-                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[20px]">Status : </h1> <p className="font-medium capitalize text-[15px] text-textColor">For Rent</p></span>
+                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[12px] md:text-[20px]">Rooms : </h1> <p className="font-medium capitalize text-[12px] text-textColor">40</p></span>
+                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[12px] md:text-[20px]">Baths : </h1> <p className="font-medium capitalize text-[12px] text-textColor">40</p></span>
+                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[12px] md:text-[20px]">Year built : </h1> <p className="font-medium capitalize text-[12px] text-textColor">2024</p></span>
+                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[12px] md:text-[20px]"> Beds : </h1> <p className="font-medium capitalize text-[12px] text-textColor">32</p></span>
+                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[12px] md:text-[20px]">Price : </h1> <p className="font-medium capitalize text-[12px] text-textColor">N 400000</p></span>
+                            <span className="flex items-center gap-1"><h1 className="uppercase text-textTitle font-bold text-[12px] md:text-[20px]">Status : </h1> <p className="font-medium capitalize text-[12px] text-textColor">For Rent</p></span>
                         </div>
                     </div>
 
                
                 <div className="flex flex-col gap-5">
-                <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[30px] ">Amenities</h1>
+                <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[15px] md:text-[30px] ">Amenities</h1>
                 <div className="grid grid-cols-3 gap-5">
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
-                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[15px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[15px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
+                    <span className="flex items-center gap-1"><FaCheckCircle className="text-btn-primary text-[12px] md:text-[20px] lg:text-[20px]" /> <p className="font-medium capitalize text-[12px] text-textColor">Microwave</p></span>
                 </div>
                     </div>
                     
@@ -119,8 +126,8 @@ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur expedita v
 
                 <div className="flex flex-col gap-[50px]">
                     <div className="flex flex-col gap-5">
-                    <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[30px] ">Map</h1>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.630659343489!2d7.493425073649227!3d6.441451324114151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1044a3d916d74623%3A0xbc5e0380e77941ad!2s1a%20Denton%20St%2C%20Ogui%2C%20Enugu%20400102%2C%20Enugu!5e0!3m2!1sen!2sng!4v1723050537379!5m2!1sen!2sng" width="full" className='w-full' height="700"   loading="lazy" ></iframe>
+                    <h1 className="font-bold px-[10px] border-l-[5px] border-btn-primary text-textTitle text-[15px]  md:text-[30px] ">Map</h1>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.630659343489!2d7.493425073649227!3d6.441451324114151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1044a3d916d74623%3A0xbc5e0380e77941ad!2s1a%20Denton%20St%2C%20Ogui%2C%20Enugu%20400102%2C%20Enugu!5e0!3m2!1sen!2sng!4v1723050537379!5m2!1sen!2sng" width="full" className='w-full' height="400"   loading="lazy" ></iframe>
                     </div>
                 </div>
                 
--- pages/my-account/my-order/index.tsx
+++ pages/my-account/my-order/index.tsx
@@ -13,42 +13,40 @@ export default function MyOrder() {
             <MobileNav />
             <DesktopNav />
             <div className="px-[30px] py-[100px]">
-                <div className="grid gap-[50px] grid-cols-6 lg:grid-cols-3">
+                <div className="grid gap-[50px] grid-cols-10 lg:grid-cols-3">
                     <MyAccountNav />
                     <div></div>
-                    <div className="col-span-5 lg:col-span-2 flex flex-col px-[20px] gap-5 h-fit py-[30px]">
-                        <h1 className="bg-titleBg w-fit px-[20px] py-[10px] text-btn-primary text-[20px] uppercase font-semibold">
-                            My Order
-                        </h1>
+                    <div className="col-span-9 lg:col-span-2 flex flex-col px-[20px] gap-5 h-fit py-[30px]">
+                    <h1 className="bg-titleBg w-fit px-[20px] py-[10px] text-btn-primary text-[15px] md:text-[20px] uppercase font-semibold ">My Order</h1>
                         <div className="overflow-x-auto">
                             <table className="w-fit overflow-x-auto  bg-white border border-gray-200">
                                 <thead>
                                     <tr className="bg-gray-200 text-gray-700">
-                                        <th className="py-2 px-4 text-left">Image</th>
-                                        <th className="py-2 px-4 text-left">Name</th>
-                                        <th className="py-2 px-4 text-left">Date</th>
-                                        <th className="py-2 px-4 text-left">Status</th>
+                                        <th className="py-2 px-4 text-[10px] md:text-[15px] text-left">Image</th>
+                                        <th className="py-2 px-4 text-[10px] md:text-[15px] text-left">Name</th>
+                                        <th className="py-2 px-4 text-[10px] md:text-[15px] text-left">Date</th>
+                                        <th className="py-2 px-4 text-[10px] md:text-[15px] text-left">Status</th>
                                         
                                     </tr>
                                 </thead>
                                 <tbody className="w-full overflow-x-auto">
                                     <tr className="border-b">
                                         <td className="py-2 px-4">
-                                            <Image src={myPic} className="w-[50px] h-[50px]" alt="Product Image" />
+                                            <Image src={myPic} className="md:w-[50px] md:h-[50px]" alt="Product Image" />
                                         </td>
-                                        <td className="py-2 px-4 text-[10px] lg:text-[13px] font-bold text-textTilte uppercase">
+                                        <td className="py-2 px-4 text-[5px] md:text-[13px] font-bold text-textTilte uppercase">
                                             The new land
                                         </td>
-                                        <td className="py-2 text-[10px] lg:text-[13px] px-4 text-textColor font-semibold">
+                                        <td className="py-2 text-[5px] md:text-[13px] px-4 text-textColor font-semibold">
                                             Aug, 10, 2024
                                         </td>
-                                        <td className="py-2 w-ful px-4 self-center text-textColor font-semibold flex items-center">
+                                        <td className="py-2 w-ful px-4 self-center text-textColor font-semibold text-[5px] md:text-[13px] flex items-center">
                                             <GoDotFill className="text-btn-primary mr-1" /> Pending
                                             <div className="py-2 px-4 w-full items-center flex-col lg:flex-row flex gap-2">
                                             {/* <Link href='' className="bg-red-500 min-w-[80px]  text-light p-1 text-[13px] rounded font-semibold">
                                                 Cancel Order
                                             </Link> */}
-                                            <Link href='/my-account/my-order/:id' className="bg-green-500 text-light min-w-[80px] p-1 text-[13px] rounded font-semibold">
+                                            <Link href='/my-account/my-order/:id' className="bg-green-500 text-light min-w-[40px] p-1 text-[5px] md:text-[13px] rounded font-semibold">
                                                 View Order
                                             </Link>
                                         </div>
