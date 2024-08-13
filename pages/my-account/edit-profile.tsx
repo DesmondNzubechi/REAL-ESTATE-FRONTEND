9c09a65800b0cc412a3b48893042b3c29eae6405
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-08-13 03:35:56 +0100

    new change

--- pages/my-account/edit-profile.tsx
+++ pages/my-account/edit-profile.tsx
@@ -17,39 +17,39 @@ export default function EditProfile() {
         <MobileNav/>
         <DesktopNav/>
     <div className="px-[30px] py-[100px] ">
-       <div className="grid gap-[50px] grid-cols-6 lg:grid-cols-3">
+       <div className="grid gap-[50px] grid-cols-10 lg:grid-cols-3">
             <MyAccountNav />
             <div></div>
-            <div className="col-span-5 lg:col-span-2 flex flex-col  px-[20px] gap-5 h-fit py-[30px] ">
-                <h1 className="bg-titleBg w-fit px-[20px] py-[10px] text-btn-primary text-[20px] uppercase font-semibold ">Edit profile</h1>
+            <div className="col-span-9 lg:col-span-2 flex flex-col  px-[20px] gap-5 h-fit py-[30px] ">
+                <h1 className="bg-titleBg w-fit px-[20px] py-[10px] text-btn-primary text-[15px] md:text-[20px] uppercase font-semibold ">Edit profile</h1>
             <form action="" className="grid grid-cols-1 md:grid-cols-2 w-full gap-5">
             <div className='border-[2px] flex w-full justify-between px-[20px] py-[20px] '>
-                        <input type="text" name="firstname" placeholder='First name' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="text" name="firstname" placeholder='First name' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                 </div>
                 <div className='border-[2px] w-full flex justify-between px-[20px] py-[20px] '>
-                        <input type="text" name="lastname" placeholder='Last name' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="text" name="lastname" placeholder='Last name' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                 </div>
                 <div className='border-[2px] flex w-full justify-between px-[20px] py-[20px] '>
-                        <input type="text" name="username" placeholder='Username' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="text" name="username" placeholder='Username' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                 </div>
                 <div className='border-[2px] flex w-full justify-between px-[20px] py-[20px] '>
-                        <input type="tel" name="phone no" placeholder='+2347084183...' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="tel" name="phone no" placeholder='+2347084183...' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                 </div>
                 <div className='border-[2px] flex w-full justify-between px-[20px] py-[20px] '>
-                        <input type="text" name="country" placeholder='Country' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="text" name="country" placeholder='Country' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                 </div>
                 <div className='border-[2px] flex w-full justify-between px-[20px] py-[20px] '>
-                        <input type="text" name="state" placeholder='State' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="text" name="state" placeholder='State' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                                             </div>
                                             
             <div className='border-[2px] flex  w-full justify-between px-[20px] py-[20px] '>
-                        <input type="email" name="email" placeholder='nzubechukwu@gmail.com' className='text-btn2 w-full bg-transparent outline-0 text-[20px] ' /> 
+                        <input type="email" name="email" placeholder='nzubechukwu@gmail.com' className='text-btn2 w-full bg-transparent outline-0 text-[15px] md:text-[20px] ' /> 
                         
                                             </div>
                                             <div className="items-center flex relative">
@@ -61,7 +61,7 @@ export default function EditProfile() {
                                                                  <MdEdit className="bg-secondaryBg border text-btn-primary rounded-full"/>
                                                              </label>
                                                             </div> 
-                <button className='text-light md:col-span-2 uppercase w-fit bg-btn-primary px-[30px] py-[15px] font-bold hover:bg-textTitle hover:text-light '>register</button>
+                <button className='text-light md:col-span-2 text-[15px] md:text-[20px] uppercase w-fit bg-btn-primary px-[30px] py-[15px] font-bold hover:bg-textTitle hover:text-light '>register</button>
             </form>
         </div>
     </div>
--- pages/my-account/index.tsx
+++ pages/my-account/index.tsx
@@ -14,36 +14,36 @@ export default function MyAccount() {
     <MobileNav/>
     <DesktopNav/>
     <div className="px-[30px] pt-[150px] py-[100px] ">
-       <div className="grid gap-[50px] grid-cols-6 lg:grid-cols-3">
+       <div className="grid gap-[50px] grid-cols-10 lg:grid-cols-3">
             <div></div>
         <MyAccountNav />
-        <div className="lg:col-span-2 col-span-5 border flex flex-col lg:flex-row px-[20px] gap-5 h-fit py-[30px] ">
+        <div className="lg:col-span-2 col-span-9 border flex flex-col lg:flex-row px-[20px] gap-5 h-fit py-[30px] ">
             <div>
                 <Image src={myPic} className="w-[300px] " alt="user image" />
             </div>
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col ">
-                    <h1 className="font-bold capitalize text-[25px] text-textTitle">Desmond Nzubechukwu</h1>
-                    <h2 className="font-semibold text-textColor text-[20px] ">@NzubechukwuDev</h2>
+                    <h1 className="font-bold capitalize text-[15px] md:text-[25px] text-textTitle">Desmond Nzubechukwu</h1>
+                    <h2 className="font-semibold text-textColor text-[12px] md:text-[20px] ">@NzubechukwuDev</h2>
                     </div>
                 <div className="flex flex-col gap-5">
                     <span className="flex items-center gap-3">
-                        <FaLocationDot className="text-btn-primary text-[20px] "/>
-                        <p className="text-textColor text-[17px] ">Enugu, Nigeria</p>
+                        <FaLocationDot className="text-btn-primary text-[15px] md:text-[20px] "/>
+                        <p className="text-textColor text-[12px] md:text-[15px] ">Enugu, Nigeria</p>
                         </span>  
 
                         <span className="flex items-center gap-3">
-                        <MdEmail className="text-btn-primary text-[20px] "/>
-                        <p className="text-textColor text-[17px] ">desmondnzubechukwu1@gmail.com</p>
+                        <MdEmail className="text-btn-primary text-[15px] md:text-[20px] "/>
+                        <p className="text-textColor text-[12px] md:text-[15px] ">desmondnzubechukwu1@gmail.com</p>
                         </span>  
 
                         <span className="flex items-center gap-3">
-                        <FaFlag className="text-btn-primary text-[20px] "/>
-                        <p className="text-textColor text-[17px] ">Nigeria</p>
+                        <FaFlag className="text-btn-primary text-[15px] md:text-[20px] "/>
+                        <p className="text-textColor text-[12px] md:text-[15px] ">Nigeria</p>
                         </span>  
                         <span className="flex items-center gap-3">
-                        <IoCall className="text-btn-primary text-[20px] "/>
-                        <p className="text-textColor text-[17px] ">+2347084183611</p>
+                        <IoCall className="text-btn-primary text-[15px] md:text-[20px] "/>
+                        <p className="text-textColor text-[12px] md:text-[15px] ">+2347084183611</p>
                     </span>  
                 </div>
             </div>
