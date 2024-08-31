1ea2a7e71d40c3f43cc95369b440e684978ba84f
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-08-31 23:42:15 +0100

    new change

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -25,28 +25,28 @@ export const Footer = () => {
                         <Image src={logo} alt="logo"/>
                     </Link>
                 {/* <h1 className="font-bold text-[25px] capitalize mb-[5px] ">b2r-tech</h1> */}
-                <p>we bring your ideas to life with technology. Our expert team turns concepts into impactful digital solutions, ensuring excellence every step of the way. Partner with us to drive your success.</p>
+                <p>We bring your real estate visions to life with innovative solutions. Our expert team transforms your property goals into reality, ensuring excellence at every stage. Partner with us to achieve your real estate success and find the perfect property or maximize your investment.</p>
             </div>
             <div className="flex flex-col gap-1">
                 <h1 className="font-bold text-[25px] capitalize mb-[5px]">Company</h1>
                 <div className="flex flex-col gap-2 ">
                     <Link className="text-slate-300 text-[15px]" href='/'>Home</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/About'>About Us</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/Contact'>Contact Us</Link>
-                        <Link className="text-slate-300 text-[15px]" href='/Team'>Properties</Link>
-                        <Link className="text-slate-300 text-[15px]" href='/About'>Land</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/Contact'>Blog</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/about'>About Us</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/contact-us'>Contact Us</Link>
+                        <Link className="text-slate-300 text-[15px]" href='/properties'>Properties</Link>
+                        <Link className="text-slate-300 text-[15px]" href='/land'>Land</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/blog'>Blog</Link>
                   
                 </div>
             </div>
             <div className="flex flex-col gap-1">
                 <h1 className="font-bold text-[25px] capitalize mb-[5px]">Our Services</h1>
                 <div className="flex flex-col gap-2 ">
-                    <Link className="text-slate-300 text-[15px]" href='/Services/Web-Decelopment'>Buy Properties</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/Services/Video-Editing'>Sell Properties</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/Services/Content-Writing'>Rent Properties</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/Services/Graphic-Design'>Buy Land</Link>
-                    <Link className="text-slate-300 text-[15px]" href='/Services/Motion-Graphics'>Sell Land</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/properties'>Buy Properties</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/properties'>Sell Properties</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/properties'>Rent Properties</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/land'>Buy Land</Link>
+                    <Link className="text-slate-300 text-[15px]" href='/land'>Sell Land</Link>
                    
                 </div>
             </div>
@@ -54,13 +54,13 @@ export const Footer = () => {
                 <h1 className="font-bold text-[25px] capitalize mb-[5px]">Get In Touch</h1>
                 <div className="flex flex-col gap-2 ">
                    <a href="mailto:"></a>
-                    <Link className="text-slate-300 text-[15px]" href='mailto:dexdigit1@gmail.com'>ceo@homefeatures.com</Link>
+                    <Link className="text-slate-300 text-[15px]" href=''>ceo@homefeatures.com</Link>
                     <Link className="text-slate-300 text-[15px]" href='tel:+2347084183611'>+234 708 4183 0000</Link>
                     <div className="flex lex-row gap-2">
-                        <Link href='https://www.linkedin.com/company/dexdigit/'><FaLinkedin /></Link>
-                        <Link href='https://www.facebook.com/profile.php?id=61561997349461'><FaFacebookSquare /></Link>
-                        <Link href='https://x.com/DexDigit'><FaXTwitter /></Link>  
-                    <Link href='https://www.instagram.com/dexdigit_?igsh=N3E2cHM2M3hqaTl0'><FaInstagramSquare /></Link>    
+                        <Link href=''><FaLinkedin /></Link>
+                        <Link href=''><FaFacebookSquare /></Link>
+                        <Link href=''><FaXTwitter /></Link>  
+                    <Link href=''><FaInstagramSquare /></Link>    
     </div>
                 </div>
             </div>
