60c6c962b16e93ab5d2c8f6c7e349de23f6c5984
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2024-09-08 16:27:19 +0100

    made few changes

--- components/overview/Overview.tsx
+++ components/overview/Overview.tsx
@@ -7,26 +7,14 @@ export const Overview = () => {
             <div className="bg-secondaryBg shadow-xl w-full grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-4 border py-[50px] md:py-[100px] p-[20px] ">
                 <select name="" id="" className=" px-[25px] outline-0 text-center shadow font-medium text-[15px] px-[20px] md:text-[25px] py-[20px] ">
                     <option>Choose Location</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
                 </select>
                 <select name="" id="" className=" px-[25px] outline-0 text-center shadow font-medium text-[15px] px-[20px] md:text-[25px] py-[20px] ">
-                    <option>Choose Location</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
+                    <option>Choose price</option>
                 </select>
                 <select name="" id="" className=" px-[25px] outline-0 text-center shadow font-medium text-[15px] px-[20px] md:text-[25px] py-[20px] ">
-                    <option>Choose Location</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
-                    <option value="Enugu">Enugu</option>
+                    <option>Choose Type</option>
                 </select>
-                <Link href='' className="bg-btn-primary text-[15px] md:text-[20px] text-light hover:text-[#FFFFFF] hover:bg-textTitle px-[25px] text-center w-full py-[20px] ">Search</Link>
+                <Link href='/properties' className="bg-btn-primary text-[15px] md:text-[20px] text-light hover:text-[#FFFFFF] hover:bg-textTitle px-[25px] text-center w-full py-[20px] ">Search</Link>
 </div>
         </div>
     </>
